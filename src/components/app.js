ca39dfc81de53e234ded90b72bdddaf803c55e3b
Sameer Iyengar <sameer.m.iyengar@gmail.com>
2024-10-16 06:14:37 -0700

    Update components

---
+++ src/components/app.js
@@ -0,0 +1,23 @@
+export default function manifest() {
+  return {
+    name: 'Next.js PWA',
+    short_name: 'NextPWA',
+    description: 'A Progressive Web App built with Next.js',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
